13ffaecfb44e7f840bd2d8ae3a0380c48831f673
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-14 07:51:35 -0600

    First commit, go_router, appTheme, initial folders

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+//órden de importaciones
+//1.- Importaciones de Dart
+//2.- Importaciones de Flutter
+//3.- Importaciones de terceros
+//4.- Dependencias
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
