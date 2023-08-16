695b5d1d93db9ba32cd0d9526dbe32cf8a5bdfcb
Alfredo-JT <21030761itcelaya.edu.mx>
2023-08-16 14:26:31 -0600

    Using the data fetched get form provider to render info and Changed the exports errors

--- lib/domain/repositories/movies_repository.dart
+++ lib/domain/repositories/movies_repository.dart
@@ -1,6 +1,5 @@
 import 'package:cinemapedia/domain/entities/movies.dart';
 
 abstract class MoviesRepository {
-  @override
   Future<List<Movie>> getNowPlay({int page = 1});
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,17 +1,44 @@
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   static const name = 'home-screen';
 
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Environment.theMovieDBKey),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Scaffold(
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  __HomeViewState createState() => __HomeViewState();
+}
+
+class __HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final moviesOnXPage = ref.watch(nowPlayingProvider);
+
+    return Center(
+        child: ListView.builder(
+      itemCount: moviesOnXPage.length,
+      itemBuilder: (context, index) {
+        return Text(moviesOnXPage[index].posterPath);
+      },
+    ));
   }
 }
