480cb2752958930360cd219b7e879cb2287f91ea
Alfredo-JT <21030761itcelaya.edu.mx>
2023-08-16 11:35:01 -0600

    Mapper of MovieFromMovieDB created

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movies.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_from_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MoviefromMovieDB moviefromMovieDB) => Movie(
+        adult: moviefromMovieDB.adult,
+        backdropPath: moviefromMovieDB.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500/${moviefromMovieDB.backdropPath}'
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png',
+        genreIds: moviefromMovieDB.genreIds
+            .map((genreID) => genreID.toString())
+            .toList(),
+        id: moviefromMovieDB.id,
+        originalLanguage: moviefromMovieDB.originalLanguage,
+        originalTitle: moviefromMovieDB.originalTitle,
+        overview: moviefromMovieDB.overview,
+        popularity: moviefromMovieDB.popularity,
+        posterPath: moviefromMovieDB.posterPath,
+        releaseDate: moviefromMovieDB.releaseDate,
+        title: moviefromMovieDB.title,
+        video: moviefromMovieDB.video,
+        voteAverage: moviefromMovieDB.voteAverage,
+        voteCount: moviefromMovieDB.voteCount,
+      );
+}
