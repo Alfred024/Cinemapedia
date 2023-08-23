b8765264e49fa7dda2c66584ebc7f94c6938c5d0
Alfredo-JT <21030761itcelaya.edu.mx>
2023-08-22 23:02:52 -0600

    New abstract method to consume MOvieID from api

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movies.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_from_moviedb.dart';
 
 class MovieMapper {
@@ -23,5 +24,26 @@ class MovieMapper {
         video: moviefromMovieDB.video,
         voteAverage: moviefromMovieDB.voteAverage,
         voteCount: moviefromMovieDB.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
+        adult: movie.adult,
+        backdropPath: movie.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500/${movie.backdropPath}'
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png',
+        genreIds: movie.genres.map((genreID) => genreID.name).toList(),
+        id: movie.id,
+        originalLanguage: movie.originalLanguage,
+        originalTitle: movie.originalTitle,
+        overview: movie.overview,
+        popularity: movie.popularity,
+        posterPath: movie.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500/${movie.posterPath}'
+            : 'no poster found',
+        releaseDate: movie.releaseDate,
+        title: movie.title,
+        video: movie.video,
+        voteAverage: movie.voteAverage,
+        voteCount: movie.voteCount,
       );
 }
--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -26,4 +26,9 @@ class MoviesRepositoryImpl extends MoviesRepository {
   Future<List<Movie>> getUpcoming({int page = 1}) {
     return moviesDataSource.getUpcoming(page: page);
   }
+
+  @override
+  Future<Movie> getMovieByID(String id) {
+    return moviesDataSource.getMovieByID(id);
+  }
 }
